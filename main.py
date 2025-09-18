a89950cdca311b0861d51c70f6d6d8300b3aa88b
darkcom109 <garciavalentino2006@gmail.com>
2025-09-18 22:27:48 +0100

    Testing prototype

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI
+# Import instance from FastAPI, Form lets us accept form data (what user pastes)
+from fastapi import FastAPI, Form, Request
 
-app = FastAPI()
+# Tells FastAPI to return HTML not JSON
+from fastapi.responses import HTMLResponse
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+# Tells FastAPI where templates live
+templates = Jinja2Templates(directory="templates")
+
+app = FastAPI(title="FastBox", version="1.0") #  Instantiate object
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Route for homepage, parameter tells FastAPI to return the string as raw HTML
+# Includes a form where users submit the data to /preview
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+    
+
+# This is the endpoint to receive form submission
+@app.post("/preview", response_class=HTMLResponse)
+def preview(request: Request, code: str = Form(...)):
+    return templates.TemplateResponse("preview.html", {"request": request, "code": code})
